cd331d164647c3206bf779af3a33a3d5b2d1223e
cheshirehat <cheshireinhatter@gmail.com>
2020-01-24 12:21:01 +0900

    add

--- trap.sh
+++ trap.sh
@@ -6,7 +6,7 @@ trap catch ERR
 
 function catch {
   echo 失敗したから失敗用の通知飛ばす
-  curl -X POST -H "Content-Type: application/json" -d '{"text":"run失敗"}' ${GITHUB_WEBHOOK_URL}
+  curl -X POST -H "Content-Type: application/json" -d '{"channel":${CHANNEL}, "text":"run失敗"}' ${GITHUB_WEBHOOK_URL}
 }
 
 ここで失敗させる
